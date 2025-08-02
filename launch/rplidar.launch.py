780b110e482fade3e78d36ffdca2e69030c0f3d1
mcluckoff <nikolai.maklakov@uni-weimar.de>
2025-08-02 22:04:26 +0200

    fixed lidar launch file

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,7 +11,8 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': 'platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1.2:1.0-port0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1.2:1.0-port0',
+                'serial_baudrate': 115200,
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'

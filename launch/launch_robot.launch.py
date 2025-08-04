33ce4ea0495b1de8883e9fa52f38a11fb8b952cb
mcluckoff <nikolai.maklakov@uni-weimar.de>
2025-08-04 15:38:59 +0200

    copied nav and slam launch, added joystick

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -28,6 +28,12 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )])
+    # )
+
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
@@ -74,6 +80,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
         delayed_controller_manager,
         delayed_ackermann_drive_spawner,
         delayed_joint_broad_spawner
